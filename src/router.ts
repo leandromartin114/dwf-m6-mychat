bee2a4e7f9de73867914eaf408493770d077a6c6
Leandro <leandrom.roldan@gmail.com>
2022-04-26 15:21:12 -0300

    router fixed

--- src/router.ts
+++ src/router.ts
@@ -1,26 +1,41 @@
 import { initHomepage } from "./pages/home";
 import { initChatpage } from "./pages/chatroom";
+
+const BASE_PATH = "/dwf-m6-mychat.herokuapp.com";
+
+function isHerokuPage() {
+	return location.host.includes("dwf-m6-mychat.herokuapp.com");
+}
+
 export function initRouter(container: Element) {
 	const routes = [
 		{ path: /\/home/, component: initHomepage },
 		{ path: /\/chatroom/, component: initChatpage },
 	];
 	function goTo(path) {
-		history.pushState({}, "", path);
+		const completePath = isHerokuPage() ? BASE_PATH + path : path;
+		history.pushState({}, "", completePath);
 		handleRoute(path);
 	}
 	function handleRoute(route) {
+		const newRoute = isHerokuPage() ? route.replace(BASE_PATH, "") : route;
 		for (const r of routes) {
-			if (r.path.test(route)) {
+			if (r.path.test(newRoute)) {
 				const element = r.component({ goTo: goTo });
 				container.firstChild?.remove();
 				container.appendChild(element);
 			}
 		}
 	}
-	if (location.pathname == "/") {
+	if (
+		location.pathname == "/" ||
+		location.pathname == "/dwf-m6-mychat.herokuapp.com/"
+	) {
 		goTo("/home");
 	} else {
 		handleRoute(location.pathname);
 	}
+	window.onpopstate = () => {
+		handleRoute(location.pathname);
+	};
 }
